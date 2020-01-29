540722c70e8ec8b612eb78d053968880bc5039ab
Yingming Luo <Yingming.luo@gmail.com>
2020-01-29 21:12:23 +0000

    Event system working

---
+++ table/Assets/Scripts/Leave_meeting_button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Leave_meeting_button : MonoBehaviour
+{
+    public Button leaveButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = leaveButton.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        Debug.Log("leaveButton::onClick");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
