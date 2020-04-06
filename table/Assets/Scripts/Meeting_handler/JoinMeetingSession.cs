fae003627f03699307028c65dd77fbe5d2311345
Yingming Luo <Yingming.luo@gmail.com>
2020-04-06 04:19:44 +0100

    fixed minor issue

--- table/Assets/Scripts/Meeting_handler/JoinMeetingSession.cs
+++ table/Assets/Scripts/Meeting_handler/JoinMeetingSession.cs
@@ -5,41 +5,5 @@ using System.Security.Cryptography;
 
 public class JoinMeetingSession : MonoBehaviour
 {
-	// private static RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-	// private static string CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	// private static uint CLIENT_ID_LENGTH = 16;
-
-	// void Start()
-	// {
-	// 	StartCoroutine(TryJoinMeeting());
-	// }
-
-	// IEnumerator TryJoinMeeting(uint meetingId)
-	// {
-	// 	string url = String.Format("http://localhost:8080/meeting-session/join/{0}", meetingId);
-	// 	string clientId = GenerateClientId();
-	// 	var requestPayload = new JoinMeetingSessionRequest(clientId);
-	// 	var requestPayloadJson = JsonUtility.ToJson(requestPayload);
-	// 	UnityWebRequest www = UnityWebRequest.Post(url, requestPayloadJson);
-	// 	yield return www.SendWebRequest();
-
-	// 	if (www.isNetworkError || www.isHttpError)
-	// 	{
-	// 		Debug.Log(www.error);
-	// 	}
-	// 	else
-	// 	{
-	// 		Debug.Log("Successfully joined meeting session " + meetingId);
-	// 	}
-	// }
-
-	// private string GenerateClientId()
-	// {
-	// 	var buffer = new char[CLIENT_ID_LENGTH];
-	// 	for (int i = 0; i < CLIENT_ID_LENGTH; i++)
-	// 	{
-	// 		buffer[i] = CHARS[rng.Next(CHARS.Length)];
-	// 	}
-	// 	return new String(buffer);
-	// }
+	
 }
